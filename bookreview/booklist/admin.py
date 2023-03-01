7cce105be6dc0a9c885d28c44a4d31d90dfc8c4f
zhihua-z <zhihuazhihuazhihuazhihua@hotmail.com>
2023-03-01 23:11:02 +0800

    added more information to the models

--- bookreview/booklist/admin.py
+++ bookreview/booklist/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Book, Comment
+
+class CommentInline(admin.StackedInline):
+    model = Comment
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'publish_date']
+    fieldsets = [
+       (None,       {'fields' : ['title', 'author', 'genre', 'wordcount']}),
+       ('Time',     {'fields' : ['publish_date'], 'classes' : ['collapse']}) 
+    ]
+    inlines = [CommentInline]
+
+admin.site.register(Book, BookAdmin)
+# admin.site.register(Comment)
--- bookreview/booklist/models.py
+++ bookreview/booklist/models.py
@@ -2,5 +2,20 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    book_title = models.CharField(max_length = 200)
+    title = models.CharField(max_length = 200)
+    author = models.CharField(max_length = 200)
+    genre = models.CharField(max_length = 200)
+    wordcount = models.IntegerField()
     publish_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    book = models.ForeignKey(Book, on_delete = models.CASCADE)
+    author = models.CharField(max_length = 200)
+    content = models.CharField(max_length = 5000)
+    publish_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.book.title + ' | ' + self.author + ' | ' + self.content[:20]
